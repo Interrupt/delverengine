16c561a849b20c4dabb6f08996463db84d706176
Joshua Skelton <joshua.skelton@gmail.com>
2020-03-09 22:00:11 -0700

    Splash screen documentation + JSON schema

--- Dungeoneer/src/com/interrupt/dungeoneer/screens/SplashScreenInfo.java
+++ Dungeoneer/src/com/interrupt/dungeoneer/screens/SplashScreenInfo.java
@@ -4,10 +4,22 @@ import com.badlogic.gdx.graphics.Color;
 
 public class SplashScreenInfo {
     public SplashScreenInfo() { }
+
+    /** Background level filepath. */
     public String backgroundLevel = "levels/temple-splash.bin";
+
+    /** Background image filepath. */
     public String backgroundImage = "splash/Delver-Menu-BG.png";
+
+    /** Splash screen logo filepath. */
     public String logoImage = "splash/Delver-Logo.png";
+
+    /** Splash screen music filepath. */
     public String music = "title.mp3";
+
+    /** Fog ending distance for background level. */
     public float fogEnd = 15;
+
+    /** Fog start distance for background level. */
     public float fogStart = 3;
 }
