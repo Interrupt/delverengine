18367e3b59a79b186a0fc27531c658134272aaa3
Joshua Skelton <joshua.skelton@gmail.com>
2020-03-11 13:02:26 -0700

    HD logo

--- Dungeoneer/src/com/interrupt/dungeoneer/screens/SplashScreenInfo.java
+++ Dungeoneer/src/com/interrupt/dungeoneer/screens/SplashScreenInfo.java
@@ -7,6 +7,7 @@ public class SplashScreenInfo {
     public String backgroundLevel = "levels/temple-splash.bin";
     public String backgroundImage = "splash/Delver-Menu-BG.png";
     public String logoImage = "splash/Delver-Logo.png";
+	public boolean logoFilter = false;
     public String music = "title.mp3";
     public float fogEnd = 15;
     public float fogStart = 3;
