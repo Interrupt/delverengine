adac0b7d292f5e11a3409bf496afe0a3bb3cefb8
Joshua Skelton <joshua.skelton@gmail.com>
2020-03-29 14:08:55 -0700

    Support delete key but don't duplicate menu entries

--- DelvEdit/src/com/interrupt/dungeoneer/editor/ui/EditorUi.java
+++ DelvEdit/src/com/interrupt/dungeoneer/editor/ui/EditorUi.java
@@ -225,6 +225,8 @@ public class EditorUi {
             .addItem(new MenuItem("Exit", smallSkin, editor.exitAction))
         );
 
+        MenuItem.acceleratorItems.add(new MenuItem("Delete", smallSkin, editor.deleteAction).setAccelerator(new MenuAccelerator(Keys.FORWARD_DEL, false, false)));
+
         menuBar.addItem(new MenuItem("Edit", smallSkin)
                         .addItem(new MenuItem("Undo", smallSkin, editor.undoAction).setAccelerator(new MenuAccelerator(Keys.Z, true, false)))
                         .addItem(new MenuItem("Redo", smallSkin, editor.redoAction).setAccelerator(new MenuAccelerator(Keys.Y, true, false)))
@@ -234,7 +236,6 @@ public class EditorUi {
                         .addItem(new MenuItem("Carve Tiles", smallSkin, editor.carveAction).setAccelerator(new MenuAccelerator(Keys.ENTER, false, false)))
                         .addItem(new MenuItem("Paint Tiles", smallSkin, editor.paintAction).setAccelerator(new MenuAccelerator(Keys.ENTER, false, true)))
                         .addItem(new MenuItem("Delete", smallSkin, editor.deleteAction).setAccelerator(new MenuAccelerator(Keys.DEL, false, false)))
-                        .addItem(new MenuItem("Delete", smallSkin, editor.deleteAction).setAccelerator(new MenuAccelerator(Keys.FORWARD_DEL, false, false)))
                         .addItem(new MenuItem("Pick Textures", smallSkin, pickAction).setAccelerator(new MenuAccelerator(Keys.G, false, false)))
                         .addItem(new MenuItem("Reset Selection", smallSkin, editor.escapeAction).setAccelerator(new MenuAccelerator(Keys.ESCAPE, false, false)))
                         .addSeparator()
