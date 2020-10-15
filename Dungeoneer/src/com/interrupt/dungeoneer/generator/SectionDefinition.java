9d16055759282b0afd9872de5abefeb914032f80
Felix Siebeneicker <felix.sieben@t-online.de>
2020-10-15 15:45:12 +0200

    Refactoring SectionDefinition class

--- Dungeoneer/src/com/interrupt/dungeoneer/generator/SectionDefinition.java
+++ Dungeoneer/src/com/interrupt/dungeoneer/generator/SectionDefinition.java
@@ -5,8 +5,6 @@ import com.interrupt.dungeoneer.game.Level;
 import com.interrupt.dungeoneer.serializers.KryoSerializer;
 
 public class SectionDefinition {
-    public SectionDefinition() { }
-
     /** What kind of loot should we drop here? */
     public int difficultyLevel = 1;
 
@@ -19,36 +17,78 @@ public class SectionDefinition {
     /** Number of levels in this section. */
     public int floors = 2;
 
-    /** Transition level to use after all floors in this section. If numFloors is 0, just use this level. */
+    /** Transition level to use after all floors in this section. If `floors` is 0, just use this level. */
     public Level transitionLevel = null;
 
     /** Array of possible level templates to use when making floors. */
     public Array<Level> levelTemplates = null;
 
+    /** Returns an array of level templates, ordered by floors, transition level last. */
     public Array<Level> buildLevels() {
-        Array<Level> l = new Array<Level>();
+        Array<Level> levels = new Array<>();
 
-        // make the levels for this dungeon section
-        for(int i = 0; i < floors; i++) {
-            if(levelTemplates != null && levelTemplates.size > 0) {
-                Level picked = levelTemplates.first();
-                Level copy = (Level)KryoSerializer.copyObject(picked);
-
-                if(copy.levelName == null || copy.levelName.equals(""))
-                    copy.levelName = name + " " + (i + 1);
-
-                copy.dungeonLevel = difficultyLevel + i;
-                l.add(copy);
+        if(hasFloors() && hasLevelTemplates()) {
+            for(int floor = 0; floor < floors; floor++) {
+                levels.add(pickLevelTemplate(floor));
             }
         }
 
-        // add a transition level if one was set
-        if(transitionLevel != null) {
-            Level copy = (Level)KryoSerializer.copyObject(transitionLevel);
-            copy.dungeonLevel = difficultyLevel + floors;
-            l.add(copy);
+        if (hasTransitionLevel()) {
+            levels.add(pickTransitionLevel());
         }
 
-        return l;
+        return levels;
+    }
+
+    /** Picks a level template. */
+    private Level pickLevelTemplate(int floor) {
+        Level levelTemplate = levelTemplates.first();
+        Level level = copyLevelDefinition(levelTemplate);
+
+        level.levelName = getLevelName(level, floor);
+        level.dungeonLevel = getDungeonLevel(floor);
+
+        return level;
+    }
+
+    /** Picks a transition level. */
+    private Level pickTransitionLevel() {
+        Level level = copyLevelDefinition(transitionLevel);
+        level.dungeonLevel = getDungeonLevel(floors);
+        return level;
+    }
+
+    /** Returns the level name. */
+    private String getLevelName(Level level, int floor) {
+        if(level.levelName == null || level.levelName.equals("")) {
+            return name + " " + (floor + 1);
+        }
+
+        return level.levelName;
+    }
+
+    /** Returns the dungeon level. */
+    private int getDungeonLevel(int floor) {
+        return difficultyLevel + floor;
+    }
+
+    /** Returns a copy of a level definition. */
+    private Level copyLevelDefinition(Level level) {
+        return (Level)KryoSerializer.copyObject(level);
+    }
+
+    /** Does the `SectionDefinition` have at least one level template? */
+    private boolean hasLevelTemplates() {
+        return levelTemplates != null && levelTemplates.size > 0;
+    }
+
+    /** Does the `SectionDefinition` have at least one floor? */
+    private boolean hasFloors() {
+        return floors > 0;
+    }
+
+    /** Does the `SectionDefinition` have a transition level? */
+    private boolean hasTransitionLevel() {
+        return transitionLevel != null;
     }
 }
