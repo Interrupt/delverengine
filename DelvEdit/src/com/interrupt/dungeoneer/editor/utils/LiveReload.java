0e8eb49ab04e8a09f656d44968df66f020e750b7
Joshua Skelton <joshua.skelton@gmail.com>
2020-10-30 21:19:00 -0700

    Live Reload Now Watches Subdirectories
    * Live Reload now watches recursively
    
    * Fixing issue with Prefab force refresh

--- DelvEdit/src/com/interrupt/dungeoneer/editor/utils/LiveReload.java
+++ DelvEdit/src/com/interrupt/dungeoneer/editor/utils/LiveReload.java
@@ -4,7 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.interrupt.dungeoneer.editor.Editor;
 import com.interrupt.dungeoneer.editor.EditorArt;
 
+import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -25,74 +27,81 @@ public class LiveReload {
     private final List<String> watchedExtensions = Arrays.asList("dat", "obj", "png", "frag", "vert");
 
     public LiveReload() {
-    	if (LiveReload.created) {
-    		throw new RuntimeException("FileWatcher instance already exists.");
-		}
+        if (LiveReload.created) {
+            throw new RuntimeException("FileWatcher instance already exists.");
+        }
 
-        watcher = new Thread() {
-			public void run() {
-				try {
-					WatchService watchService = FileSystems.getDefault().newWatchService();
-					java.nio.file.Path path = Paths.get(Gdx.files.getLocalStoragePath());
-					path.register(watchService, ENTRY_CREATE, ENTRY_MODIFY);
+        watcher = new Thread(() -> {
+            try {
+                WatchService watchService = FileSystems.getDefault().newWatchService();
+                Path root = Paths.get(Gdx.files.getLocalStoragePath());
+                root.register(watchService, ENTRY_CREATE, ENTRY_MODIFY);
 
-					boolean running = true;
-					while(running) {
-						WatchKey key = watchService.take();
-						for (WatchEvent<?> event : key.pollEvents()) {
-							@SuppressWarnings("unchecked")
-							WatchEvent<Path> watchEvent = (WatchEvent<Path>)event;
-							String filename = watchEvent.context().toString();
-							String extension = getFileExtension(filename).toLowerCase();
+                Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                        dir.register(watchService, ENTRY_CREATE, ENTRY_MODIFY);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
 
-							long now = System.currentTimeMillis();
+                boolean running = true;
+                while (running) {
+                    WatchKey key = watchService.take();
+                    for (WatchEvent<?> event : key.pollEvents()) {
+                        @SuppressWarnings("unchecked")
+                        WatchEvent<Path> watchEvent = (WatchEvent<Path>) event;
+                        String filename = watchEvent.context().toString();
+                        String extension = getFileExtension(filename).toLowerCase();
 
-							if (watchedExtensions.contains(extension) && now - lastTimeCalled > MIN_TIME_BETWEEN_CALLS) {
-								lastTimeCalled = now;
+                        long now = System.currentTimeMillis();
 
-								// Reload must happen on main render thread.
-								needToReloadAssets.set(true);
-								break;
-							}
-						}
+                        if (watchedExtensions.contains(extension) && now - lastTimeCalled > MIN_TIME_BETWEEN_CALLS) {
+                            lastTimeCalled = now;
 
-						running = key.reset();
-					}
-				}
-				catch (Exception ignore) {}
-			}
-		};
+                            // Reload must happen on main render thread.
+                            needToReloadAssets.set(true);
+                            break;
+                        }
+                    }
+
+                    running = key.reset();
+                }
+            } catch (Exception ignore) {}
+        });
     }
 
     public void init() {
-		Gdx.app.log("LiveReload", "Initializing");
-    	if (watcher.isAlive()) {
-    		return;
-		}
+        Gdx.app.log("LiveReload", "Initializing");
+        if (watcher.isAlive()) {
+            return;
+        }
 
-    	watcher.setPriority(Thread.MIN_PRIORITY);
-		watcher.start();
-	}
+        watcher.setPriority(Thread.MIN_PRIORITY);
+        watcher.start();
+    }
 
     public void dispose() {
-    	Gdx.app.log("LiveReload", "Disposing");
-    	watcher.interrupt();
-	}
+        Gdx.app.log("LiveReload", "Disposing");
+        watcher.interrupt();
+    }
 
-	public void tick() {
+    public void tick() {
         if (needToReloadAssets.compareAndSet(true, false)) {
-        	Gdx.app.log("LiveReload", "Reloading assets");
+            Gdx.app.log("LiveReload", "Reloading assets");
             EditorArt.refresh();
             Editor.app.initTextures();
+            Editor.app.loadEntities();
+            Editor.app.loadMonsters();
         }
     }
 
-	private String getFileExtension(String filename) {
-    	int index = filename.lastIndexOf(".");
-    	if (index != -1 && index != 0) {
-    		return filename.substring(index + 1);
-		}
+    private String getFileExtension(String filename) {
+        int index = filename.lastIndexOf(".");
+        if (index != -1 && index != 0) {
+            return filename.substring(index + 1);
+        }
 
-    	return "";
-	}
+        return "";
+    }
 }
