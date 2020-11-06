fc0c35f4e5501dae43ddca49359d737da2bbabaa
Felix Siebeneicker <felix.sieben@t-online.de>
2020-11-06 20:08:46 +0100

    Fixing re-generate menu items not working (#171)
    * Fixing re-generate menu items not working
    
    * Adding clear method to level
    
    * Only reseting camera for new levels
    
    * Grabbing roomGeneratorChance from template
    
    * Removing hard-coded values for level dimensions

--- DelvEdit/src/com/interrupt/dungeoneer/editor/EditorApplication.java
+++ DelvEdit/src/com/interrupt/dungeoneer/editor/EditorApplication.java
@@ -55,6 +55,8 @@ import com.interrupt.dungeoneer.game.Game;
 import com.interrupt.dungeoneer.game.Level;
 import com.interrupt.dungeoneer.game.Level.Source;
 import com.interrupt.dungeoneer.generator.DungeonGenerator;
+import com.interrupt.dungeoneer.generator.GenTheme;
+import com.interrupt.dungeoneer.generator.RoomGenerator;
 import com.interrupt.dungeoneer.generator.GenInfo.Markers;
 import com.interrupt.dungeoneer.gfx.GlRenderer;
 import com.interrupt.dungeoneer.gfx.SpriteGroupStrategy;
@@ -433,23 +435,69 @@ public class EditorApplication implements ApplicationListener {
 		}
 	}
 
+	/** Creates an empty level with given `width` and `height`. */
 	public void createEmptyLevel(int width, int height) {
 		level = new Level(width, height);
-		entireLevelSelection = TileSelection.Rect(0, 0, level.width, level.height);
-		refresh();
-		
-		history = new EditorHistory();
-		file = new EditorFile();
-		
+
 		Tile t = new Tile();
 		t.floorHeight = -0.5f;
 		t.ceilHeight = 0.5f;
 		level.setTile(width / 2, height / 2, t);
 
+		cleanEditorState();
+		cameraController.setDefaultPositionAndRotation();
+	}
+
+	/** Generates a level based on a `template` level. */
+	public void generateLevelFromTemplate(Level template) {
+		level.clear();
+
+		level.theme = template.theme;
+		level.generated = true;
+		level.dungeonLevel = 0;
+
+		GenTheme genTheme = DungeonGenerator.GetGenData(template.theme);
+		int chunkTiles = genTheme.getChunkTileSize();
+		int mapChunks = genTheme.getMapChunks();
+		level.crop(0, 0, chunkTiles * mapChunks, chunkTiles * mapChunks);
+
+		level.roomGeneratorChance = template.roomGeneratorChance;
+		level.roomGeneratorType = template.roomGeneratorType;
+		level.generate(Level.Source.EDITOR);
+
+		cleanEditorState();
+	}
+
+	/** Generates a single room based on a `template` level. */
+	public void generateRoomFromTemplate(Level template) {
+		level.clear();
+
+		GenTheme genTheme = DungeonGenerator.GetGenData(template.theme);
+		int chunkTiles = genTheme.getChunkTileSize();
+		Level generatedLevel = new Level(chunkTiles, chunkTiles);
+
+		generatedLevel.roomGeneratorType = template.roomGeneratorType;
+
+		RoomGenerator generator = new RoomGenerator(generatedLevel, template.roomGeneratorType);
+		generator.generate(true, true, true, true);
+
+		level.crop(0, 0, generatedLevel.width, generatedLevel.height);
+		level.paste(generatedLevel, 0, 0);
+		level.theme = template.theme;
+
+		cleanEditorState();
+	}
+
+	/** Should be called after manually setting the `level` in the editor. */
+	private void cleanEditorState() {
+		entireLevelSelection = TileSelection.Rect(0, 0, level.width, level.height);
+		refresh();
+		
+		history = new EditorHistory();
+		file = new EditorFile();
+
 		history.saveState(level);
 		file.markClean();
-
-		cameraController.setDefaultPositionAndRotation();
 	}
 
 	@Override
