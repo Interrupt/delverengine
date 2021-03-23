fd635b504a034a330b8f5ce1dfa686bd5faeb344
Felix Siebeneicker <felix.sieben@t-online.de>
2021-03-23 21:19:35 +0100

    Adding mod data

---
+++ Dungeoneer/src/com/interrupt/dungeoneer/modding/ModData.java
@@ -0,0 +1,15 @@
+package com.interrupt.dungeoneer.modding;
+
+public class ModData {
+    /** The title of the mod. */
+    public String title;
+
+    /** The author name of the mod. */
+    public String author;
+
+    /** Path of mod when available locally. */
+    public String path;
+
+    /** Is the mod currently enabled? */
+    public boolean enabled;
+}
