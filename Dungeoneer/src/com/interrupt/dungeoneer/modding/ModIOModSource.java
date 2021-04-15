11fa0867883dfdd9d4876add983de7ba7c7472d7
Joshua Skelton <joshua.skelton@gmail.com>
2021-04-14 22:57:15 -0700

    Extending LocalFileSystemModSource

--- Dungeoneer/src/com/interrupt/dungeoneer/modding/ModIOModSource.java
+++ Dungeoneer/src/com/interrupt/dungeoneer/modding/ModIOModSource.java
@@ -1,13 +1,11 @@
 package com.interrupt.dungeoneer.modding;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.utils.Array;
 import com.interrupt.dungeoneer.modding.modio.*;
 import com.interrupt.dungeoneer.net.ObjectResponseListener;
 import com.interrupt.dungeoneer.net.RestEndpoint;
 import com.interrupt.utils.DownloadUtils;
 
-public class ModIOModSource implements ModSource {
+public class ModIOModSource extends LocalFileSystemModSource {
     private int gameID;
     private String apiKey;
 
@@ -19,24 +17,21 @@ public class ModIOModSource implements ModSource {
     public void init() {
         api = new RestEndpoint(rootPath, apiKey);
         allModsRoute = "/games/" + gameID + "/mods";
+        root = ".mods/modio/" + gameID;
+        refresh();
     }
 
-    @Override
-    public Array<String> getInstalledMods() {
+    public void refresh() {
         api.get(allModsRoute, new ObjectResponseListener<GetMods>(GetMods.class) {
             @Override
             public void handleObjectResponse(GetMods mods) {
                 for (ModObject mod : mods.data) {
-                    Gdx.app.log("mod.io", mod.name);
-
                     String url = mod.modfile.download.binary_url;
                     String filename = mod.modfile.filename;
 
-                    DownloadUtils.downloadFile(url, ".mods//" + filename);
+                    DownloadUtils.downloadFile(url, root + "/" + filename);
                 }
             }
         });
-
-        return new Array<>();
     }
 }
