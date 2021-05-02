aaf4eb107b4a6cd7a84183d1dfae04cb201c5a5a
Joshua Skelton <joshua@recroom.com>
2021-05-02 15:42:44 -0700

    Cleaning up Android activity/manifest

--- DungeoneerAndroid/src/com/interrupt/dungeoneer/DungeoneerAndroidActivity.java
+++ DungeoneerAndroid/src/com/interrupt/dungeoneer/DungeoneerAndroidActivity.java
@@ -1,7 +1,5 @@
 package com.interrupt.dungeoneer;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -12,34 +10,6 @@ public class DungeoneerAndroidActivity extends AndroidApplication {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-
-        if (this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            this.requestPermissions(new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE }, 1000);
-        }
-
         initialize(new GameApplication(), config);
-
-        /* --- BELOW: LOWER RESOLUTION ON PHONES --- */
-    	/*super.onCreate(savedInstanceState);
-
-        // Create the layout
-        RelativeLayout layout = new RelativeLayout(this);
-
-        // Do the stuff that initialize() would do for you
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        		WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-
-        // Create the libgdx View
-        GLSurfaceView gameView = (GLSurfaceView) initializeForView(new GameApplication(), false);
-
-        gameView.getHolder().setFixedSize(840 / 6, 480 / 6);
-
-        // Add the libgdx view
-        layout.addView(gameView);
-
-        // Hook it all up
-        setContentView(layout);*/
     }
 }
