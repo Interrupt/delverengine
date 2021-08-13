abdebce970b12a8bd7db1e33695f888c1d5ed7ea
Chad Cuddigan <cuddigan@gmail.com>
2021-08-12 22:21:29 -0700

    Adding in a guard to the spatial hash to keep it from looping forever with huge input values

--- Dungeoneer/src/com/interrupt/dungeoneer/partitioning/SpatialHash.java
+++ Dungeoneer/src/com/interrupt/dungeoneer/partitioning/SpatialHash.java
@@ -8,23 +8,23 @@ import com.interrupt.dungeoneer.entities.Player;
 public class SpatialHash {
 	private final int cellSize;
 	private IntMap<Array<Entity>> hash = new IntMap<Array<Entity>>();
-	
+
 	private Array<Entity> temp = new Array<Entity>();
-	
+
 	public SpatialHash(int cellSize)  {
 		this.cellSize = cellSize;
 	}
-	
+
 	public int getKey(float x, float y) {
 		return ((int)Math.floor(x) / cellSize) + (int)((int)Math.floor(y) / cellSize) * 3000;
 	}
-	
+
 	private void PutEntity(int key, Entity e) {
 		Array<Entity> eList = hash.get(key);
 		if(eList == null) {
 			eList = new Array<Entity>();
 			eList.add(e);
-			
+
 			hash.put(key, eList);
 		}
 		else {
@@ -32,13 +32,13 @@ public class SpatialHash {
 				eList.add(e);
 		}
 	}
-	
+
 	public void AddEntity(Entity e) {
 		int startX = (int)(e.x / cellSize - e.collision.x);
 		int endX = (int)(e.x / cellSize + e.collision.x);
 		int startY = (int)(e.y / cellSize - e.collision.y);
 		int endY = (int)(e.y / cellSize + e.collision.y);
-		
+
 		for(int x = startX; x <= endX; x++) {
 			for(int y = startY; y <= endY; y++) {
 				int key = getKey(x, y);
@@ -46,14 +46,21 @@ public class SpatialHash {
 			}
 		}
 	}
-	
+
 	public Array<Entity> getEntitiesAt(float x, float y, float colSize) {
 		int startX = (int)(x / cellSize - colSize);
 		int endX = (int)(x / cellSize + colSize);
 		int startY = (int)(y / cellSize - colSize);
 		int endY = (int)(y / cellSize + colSize);
-		
-		temp.clear();
+
+        temp.clear();
+
+        // Guard from super high values causing an infinite loop here
+		if(startX == Integer.MAX_VALUE || startX == Integer.MIN_VALUE)
+		    return temp;
+        if(startY == Integer.MAX_VALUE || startY == Integer.MIN_VALUE)
+            return temp;
+
 		for(int xi = startX; xi <= endX; xi++) {
 			for(int yi = startY; yi <= endY; yi++) {
 				int key = getKey(xi, yi);
@@ -66,14 +73,14 @@ public class SpatialHash {
 				}
 			}
 		}
-		
+
 		return temp;
 	}
-	
+
 	public void Clear() {
 		hash.clear();
 	}
-	
+
 	public void Flush() {
 		for(Array<Entity> val : hash.values()) {
 			val.clear();
