ab3310baa56712e3504a828faca39d91790f69e9
Joshua Skelton <joshua.skelton@gmail.com>
2021-08-19 19:14:56 -0700

    Apply element alignment to actor

--- Dungeoneer/src/com/interrupt/dungeoneer/ui/PlayerAmmoElement.java
+++ Dungeoneer/src/com/interrupt/dungeoneer/ui/PlayerAmmoElement.java
@@ -1,13 +1,17 @@
 package com.interrupt.dungeoneer.ui;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
 import com.interrupt.dungeoneer.entities.Item;
 import com.interrupt.dungeoneer.entities.Player;
 import com.interrupt.dungeoneer.entities.items.Gun;
 import com.interrupt.dungeoneer.game.Game;
 
 public class PlayerAmmoElement extends Element {
+    public Color color = Color.WHITE;
+    
     @Override
     public Actor createActor() {
         Label label = new Label(String.valueOf(getAmmoCount()), UiSkin.getSkin()) {
@@ -25,7 +29,45 @@ public class PlayerAmmoElement extends Element {
                 }
             }
         };
-        label.setColor(75 / 255f, 91 / 255f, 143 / 255f, 1f);
+        label.setColor(color.r, color.g, color.b, 1f);
+
+        switch (pivot) {
+            case BOTTOM_LEFT:
+                label.setAlignment(com.badlogic.gdx.utils.Align.bottomLeft);
+                break;
+
+            case BOTTOM_CENTER:
+                label.setAlignment(com.badlogic.gdx.utils.Align.bottom | com.badlogic.gdx.utils.Align.center);
+                break;
+
+            case BOTTOM_RIGHT:
+                label.setAlignment(com.badlogic.gdx.utils.Align.bottomRight);
+                break;
+
+            case CENTER_LEFT:
+                label.setAlignment(com.badlogic.gdx.utils.Align.left | com.badlogic.gdx.utils.Align.center);
+                break;
+
+            case CENTER:
+                label.setAlignment(com.badlogic.gdx.utils.Align.center);
+                break;
+
+            case CENTER_RIGHT:
+                label.setAlignment(com.badlogic.gdx.utils.Align.right | com.badlogic.gdx.utils.Align.center);
+                break;
+
+            case TOP_LEFT:
+                label.setAlignment(com.badlogic.gdx.utils.Align.topLeft);
+                break;
+
+            case TOP_CENTER:
+                label.setAlignment(com.badlogic.gdx.utils.Align.top | com.badlogic.gdx.utils.Align.center);
+                break;
+
+            case TOP_RIGHT:
+                label.setAlignment(Align.topRight);
+                break;
+        }
 
         return label;
     }
--- Dungeoneer/src/com/interrupt/dungeoneer/ui/TextElement.java
+++ Dungeoneer/src/com/interrupt/dungeoneer/ui/TextElement.java
@@ -3,6 +3,7 @@ package com.interrupt.dungeoneer.ui;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
 
 public class TextElement extends Element{
     public String text = "";
@@ -13,6 +14,44 @@ public class TextElement extends Element{
         Label label = new Label(text, UiSkin.getSkin());
         label.setPosition(x, y);
         label.setColor(color.r, color.g, color.b, 1f);
+
+        switch (pivot) {
+            case BOTTOM_LEFT:
+                label.setAlignment(Align.bottomLeft);
+                break;
+
+            case BOTTOM_CENTER:
+                label.setAlignment(Align.bottom | Align.center);
+                break;
+
+            case BOTTOM_RIGHT:
+                label.setAlignment(Align.bottomRight);
+                break;
+
+            case CENTER_LEFT:
+                label.setAlignment(Align.left | Align.center);
+                break;
+
+            case CENTER:
+                label.setAlignment(Align.center);
+                break;
+
+            case CENTER_RIGHT:
+                label.setAlignment(Align.right | Align.center);
+                break;
+
+            case TOP_LEFT:
+                label.setAlignment(Align.topLeft);
+                break;
+
+            case TOP_CENTER:
+                label.setAlignment(Align.top | Align.center);
+                break;
+
+            case TOP_RIGHT:
+                label.setAlignment(Align.topRight);
+                break;
+        }
 
         return label;
     }
