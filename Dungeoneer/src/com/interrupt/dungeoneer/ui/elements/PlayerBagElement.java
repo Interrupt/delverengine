ee2897fe4a959f23388b6d3416d2aeeeee40fc17
Joshua Skelton <joshuaskelton@gmail.com>
2021-08-27 15:13:41 -0700

    Player bag element implementation

---
+++ Dungeoneer/src/com/interrupt/dungeoneer/ui/elements/PlayerBagElement.java
@@ -0,0 +1,51 @@
+package com.interrupt.dungeoneer.ui.elements;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
+import com.badlogic.gdx.utils.Align;
+import com.interrupt.dungeoneer.game.Game;
+import com.interrupt.dungeoneer.ui.InventorySlot;
+
+public class PlayerBagElement extends Element {
+    public String image;
+    public int columns = 6;
+
+    @Override
+    public Actor createActor() {
+        HorizontalGroup group = new HorizontalGroup() {
+            private int value = Game.instance.player.inventorySize;
+            @Override
+            public void act(float delta) {
+                super.act(delta);
+
+                if (value != Game.instance.player.inventorySize) {
+                    value = Game.instance.player.inventorySize;
+                    /*for (int i = value; i < Game.instance.player.hotbarSize; i++) {
+                        addActor(new InventorySlot(i, image));
+                    }
+                    Game.canvas.resize();*/
+                }
+            }
+        };
+        // Align children inside of group to the bottom left
+        group.align(Align.bottomLeft);
+        int slotWidth = 0;
+        int slotHeight = 0;
+
+        // Build out slots
+        for (int i = Game.instance.player.hotbarSize; i < Game.instance.player.inventorySize; i++) {
+            InventorySlot slot = new InventorySlot(i, image);
+            slotWidth = (int)Math.max(slotWidth, slot.getWidth());
+            slotHeight = (int)Math.max(slotHeight, slot.getHeight());
+            group.addActor(slot);
+        }
+
+        int rows = Math.round((Game.instance.player.inventorySize - Game.instance.player.hotbarSize) / (float)columns);
+        group.setHeight(slotHeight * rows);
+        group.setWidth(slotWidth * columns);
+        group.rowAlign(Align.left);
+        group.wrap();
+
+        return group;
+    }
+}
