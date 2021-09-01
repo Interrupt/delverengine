4d45b08f3a7169073c625438a9ae961a8493b6d7
Joshua Skelton <joshua.skelton@gmail.com>
2021-09-01 16:29:00 -0700

    Initial implementation of ElementPrefab

---
+++ Dungeoneer/src/com/interrupt/dungeoneer/ui/elements/ElementPrefab.java
@@ -0,0 +1,22 @@
+package com.interrupt.dungeoneer.ui.elements;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.interrupt.dungeoneer.game.Game;
+import com.interrupt.dungeoneer.serializers.KryoSerializer;
+
+public class ElementPrefab extends Element {
+    public String name;
+    private Element element;
+
+    @Override
+    protected Actor createActor() {
+        element = Game.hudManager.getPrefab(name);
+
+        if (element == null) {
+            return null;
+        }
+
+        element = (Element)KryoSerializer.copyObject(element);
+        return element.createActor();
+    }
+}
--- Dungeoneer/src/com/interrupt/managers/HUDManager.java
+++ Dungeoneer/src/com/interrupt/managers/HUDManager.java
@@ -1,10 +1,15 @@
 package com.interrupt.managers;
 
 import com.interrupt.dungeoneer.ui.Hotbar;
+import com.interrupt.dungeoneer.ui.elements.Element;
+
+import java.util.HashMap;
 
 public class HUDManager {
     public Hotbar quickSlots = new Hotbar(6, 1, 0);
     public Hotbar backpack = new Hotbar(6, 3, 6);
+
+    public HashMap<String, Element> prefabs = new HashMap<>();
 
     public void merge(HUDManager other) {
         if (null != other.quickSlots) {
@@ -14,5 +19,13 @@ public class HUDManager {
         if (null != other.backpack) {
             backpack = other.backpack;
         }
+
+        if (null != other.prefabs) {
+            prefabs = other.prefabs;
+        }
+    }
+
+    public Element getPrefab(String name) {
+        return prefabs.getOrDefault(name, null);
     }
 }
