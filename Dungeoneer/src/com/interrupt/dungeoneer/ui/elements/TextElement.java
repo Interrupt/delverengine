c784bc919216e5e7e2980021989e3d950c5e8a0d
Joshua Skelton <joshua.skelton@gmail.com>
2021-09-02 10:09:41 -0700

    Removing shadowed color field

--- Dungeoneer/src/com/interrupt/dungeoneer/ui/elements/TextElement.java
+++ Dungeoneer/src/com/interrupt/dungeoneer/ui/elements/TextElement.java
@@ -8,13 +8,11 @@ import com.interrupt.dungeoneer.ui.UiSkin;
 
 public class TextElement extends Element{
     public String text = "";
-    public Color color = Color.WHITE;
 
     @Override
     public Actor createActor() {
         Label label = new Label(text, UiSkin.getSkin());
         label.setPosition(x, y);
-        label.setColor(color.r, color.g, color.b, 1f);
 
         switch (pivot) {
             case BOTTOM_LEFT:
