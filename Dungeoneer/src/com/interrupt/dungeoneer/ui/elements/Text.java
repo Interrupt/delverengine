cbfabdff9473e6608c6c77c6c5c82d9b0103ba8f
Joshua Skelton <joshua.skelton@gmail.com>
2021-09-05 14:37:47 -0700

    Making the default Text element object dynamic

--- Dungeoneer/src/com/interrupt/dungeoneer/ui/elements/Text.java
+++ Dungeoneer/src/com/interrupt/dungeoneer/ui/elements/Text.java
@@ -2,57 +2,75 @@ package com.interrupt.dungeoneer.ui.elements;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.utils.Align;
 import com.interrupt.dungeoneer.ui.UiSkin;
+import com.interrupt.dungeoneer.ui.values.DynamicValue;
 import com.interrupt.managers.StringManager;
 
-public class Text extends Element{
-    public String text = "";
+import java.util.Objects;
+
+public class Text extends Element {
+    DynamicValue text;
+
+    private String getText() {
+        return StringManager.get(text.stringValue());
+    }
 
     @Override
-    public Actor createActor() {
-        Label label = new Label(StringManager.get(text), UiSkin.getSkin());
+    protected Actor createActor() {
+        Text self = this;
+        Label label = new Label(self.getText(), UiSkin.getSkin()) {
+            String value;
+            @Override
+            public void act(float delta) {
+                super.act(delta);
 
-        com.interrupt.dungeoneer.ui.elements.Align a = pivot;
+                String current = self.getText();
+                if (!Objects.equals(value, current)) {
+                    value = current;
+                    setText(value);
+                }
+            }
+        };
+
         if (pivot.equals(com.interrupt.dungeoneer.ui.elements.Align.UNSET)) {
             pivot = anchor;
         }
 
         switch (pivot) {
             case BOTTOM_LEFT:
-                label.setAlignment(Align.bottomLeft);
+                label.setAlignment(com.badlogic.gdx.utils.Align.bottomLeft);
                 break;
 
             case BOTTOM_CENTER:
-                label.setAlignment(Align.bottom | Align.center);
+                label.setAlignment(com.badlogic.gdx.utils.Align.bottom | com.badlogic.gdx.utils.Align.center);
                 break;
 
             case BOTTOM_RIGHT:
-                label.setAlignment(Align.bottomRight);
+                label.setAlignment(com.badlogic.gdx.utils.Align.bottomRight);
                 break;
 
             case CENTER_LEFT:
-                label.setAlignment(Align.left | Align.center);
+                label.setAlignment(com.badlogic.gdx.utils.Align.left | com.badlogic.gdx.utils.Align.center);
                 break;
 
             case CENTER:
-                label.setAlignment(Align.center);
+                label.setAlignment(com.badlogic.gdx.utils.Align.center);
                 break;
 
             case CENTER_RIGHT:
-                label.setAlignment(Align.right | Align.center);
+                label.setAlignment(com.badlogic.gdx.utils.Align.right | com.badlogic.gdx.utils.Align.center);
                 break;
 
             case TOP_LEFT:
-                label.setAlignment(Align.topLeft);
+                label.setAlignment(com.badlogic.gdx.utils.Align.topLeft);
                 break;
 
             case TOP_CENTER:
-                label.setAlignment(Align.top | Align.center);
+                label.setAlignment(com.badlogic.gdx.utils.Align.top | com.badlogic.gdx.utils.Align.center);
                 break;
 
             case TOP_RIGHT:
-                label.setAlignment(Align.topRight);
+                label.setAlignment(com.badlogic.gdx.utils.Align.topRight);
                 break;
         }
 
