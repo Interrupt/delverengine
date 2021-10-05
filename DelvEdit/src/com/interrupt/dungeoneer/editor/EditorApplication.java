38c5202410e5ec3e85b32bf4c1b956d39c0d3285
Joshua Skelton <joshua.skelton@gmail.com>
2021-10-05 16:26:48 -0700

    Removing unused gencube code

--- DelvEdit/src/com/interrupt/dungeoneer/editor/EditorApplication.java
+++ DelvEdit/src/com/interrupt/dungeoneer/editor/EditorApplication.java
@@ -235,7 +235,6 @@ public class EditorApplication implements ApplicationListener {
     protected EntityManager entityManager;
     protected MonsterManager monsterManager;
 
-	Mesh cubeMesh;
     Mesh gridMesh;
 
     private boolean slopePointMode = false;
@@ -385,7 +384,6 @@ public class EditorApplication implements ApplicationListener {
         renderer.init();
 		renderer.enableLighting = showLights;
 
-		cubeMesh = genCube();
 		spriteBatch = new DecalBatch(new SpriteGroupStrategy(camera, null, GlRenderer.worldShaderInfo, 1));
 
 		unknownEntityMarker.tex = 1000;
@@ -2416,49 +2414,6 @@ public class EditorApplication implements ApplicationListener {
 		v++;
 
 		return v;
-	}
-
-	public Mesh genCube () {
-        Mesh mesh = new Mesh(true, 24, 36, new VertexAttribute(Usage.Position, 3, "a_position"), new VertexAttribute(Usage.Normal,
-                3, "a_normal"), new VertexAttribute(Usage.TextureCoordinates, 2, "a_texcoords"));
-
-        float[] cubeVerts = {-0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f,
-                -0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f,
-                0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, -0.5f,
-                -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
-                0.5f, 0.5f, -0.5f,};
-
-        float[] cubeNormals = {0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-                1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
-                -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,
-                -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,};
-
-        float[] cubeTex = {0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
-                0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-                1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,};
-
-        float[] vertices = new float[24 * 8];
-        int pIdx = 0;
-        int nIdx = 0;
-        int tIdx = 0;
-        for (int i = 0; i < vertices.length;) {
-                vertices[i++] = cubeVerts[pIdx++];
-                vertices[i++] = cubeVerts[pIdx++];
-                vertices[i++] = cubeVerts[pIdx++];
-                vertices[i++] = cubeNormals[nIdx++];
-                vertices[i++] = cubeNormals[nIdx++];
-                vertices[i++] = cubeNormals[nIdx++];
-                vertices[i++] = cubeTex[tIdx++];
-                vertices[i++] = cubeTex[tIdx++];
-        }
-
-        short[] indices = {0, 2, 1, 0, 3, 2, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 15, 14, 12, 14, 13, 16, 17, 18, 16, 18, 19,
-                20, 23, 22, 20, 22, 21};
-
-        mesh.setVertices(vertices);
-        mesh.setIndices(indices);
-
-        return mesh;
 	}
 
 	public Mesh genGrid (int height, int width) {
