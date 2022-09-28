9647ec8685c67b2ae39ea21850da0ba8ebeb89a6
Joshua Skelton <joshua.skelton@gmail.com>
2022-09-28 15:44:25 -0700

    Fix gamepad bindings

--- Dungeoneer/src/com/interrupt/dungeoneer/input/GamepadManager.java
+++ Dungeoneer/src/com/interrupt/dungeoneer/input/GamepadManager.java
@@ -3,18 +3,17 @@ package com.interrupt.dungeoneer.input;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.ControllerListener;
+import com.badlogic.gdx.controllers.ControllerMapping;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ArrayMap;
 import com.interrupt.dungeoneer.game.Game;
 import com.interrupt.dungeoneer.game.Options;
 import com.interrupt.dungeoneer.input.ControllerState.DPAD;
 
 public class GamepadManager implements ControllerListener {
-
-	public ControllerState controllerState = null;
+	public ControllerState controllerState;
 	public Interpolation moveLerp = Interpolation.fade;
 	public Interpolation lookLerp = Interpolation.fade;
 	public boolean menuMode = false;
@@ -27,7 +26,7 @@ public class GamepadManager implements ControllerListener {
 	private Vector2 direction = new Vector2();
 	private Vector2 offset = new Vector2();
 
-	public ArrayMap<Controller, GamepadDefinition> controllerMapping = new ArrayMap<Controller, GamepadDefinition>();
+	public ArrayMap<Controller, GamepadDefinition> controllerMapping = new ArrayMap<>();
 
 	public GamepadManager(ControllerState state) {
 		controllerState = state;
@@ -128,101 +127,105 @@ public class GamepadManager implements ControllerListener {
 	}
 
 	public boolean matchButton(int index, GamepadBinding.GamepadInputType type, int mod, float value) {
-		if(Game.instance != null) Game.instance.input.usingGamepad = true;
+        if(Game.instance != null) Game.instance.input.usingGamepad = true;
 
-		for(Actions.Action action : Actions.gamepadBindings.keySet()) {
-			GamepadBinding binding = Actions.gamepadBindings.get(action);
-			if(binding != null) {
-				if(binding.number == index && binding.type == type && binding.modifier == mod) {
+        for(Actions.Action action : Actions.gamepadBindings.keySet()) {
+            GamepadBinding binding = Actions.gamepadBindings.get(action);
 
-					value = Math.abs(value);
-					boolean pressed = value > 0.25f;
+            if(binding == null) continue;
 
-					if(pressed && !isAnalogEvent(action)) {
-						// Add button events for all other digital actions
-						controllerState.buttonEvents.add(action);
-					}
+            if(binding.number == index && binding.type == type && binding.modifier == mod) {
+                value = Math.abs(value);
+                boolean pressed = value > 0.25f;
 
-					if(action == Actions.Action.ATTACK) {
-						controllerState.attack = pressed;
-					}
-					else if(action == Actions.Action.DROP) {
-						controllerState.drop = pressed;
-					}
-					else if(action == Actions.Action.USE) {
-						controllerState.use = pressed;
+                if(pressed && !isAnalogEvent(action)) {
+                    // Add button events for all other digital actions
+                    controllerState.buttonEvents.add(action);
+                }
 
-						if(value > 0.25f) {
-							controllerState.menuButtonEvents.add(ControllerState.MenuButtons.SELECT);
-						}
-					}
-					else if(action == Actions.Action.MAP) {
-						controllerState.map = pressed;
-					}
-					else if(action == Actions.Action.PAUSE) {
-						controllerState.pause = pressed;
-					}
-					else if(action == Actions.Action.INVENTORY) {
-						controllerState.inventory = pressed;
+                if(action == Actions.Action.ATTACK) {
+                    controllerState.attack = pressed;
+                }
+                else if(action == Actions.Action.DROP) {
+                    controllerState.drop = pressed;
+                }
+                else if(action == Actions.Action.USE) {
+                    controllerState.use = pressed;
 
-						if(value > 0.25f) {
-							controllerState.menuButtonEvents.add(ControllerState.MenuButtons.CANCEL);
-						}
-					}
-					else if(action == Actions.Action.FORWARD) {
-						controllerState.rawMove.y = value * mod;
-					}
-					else if(action == Actions.Action.BACKWARD) {
-						controllerState.rawMove.y = value * mod;
-					}
-					else if(action == Actions.Action.STRAFE_LEFT) {
-						controllerState.rawMove.x = value * mod;
-					}
-					else if(action == Actions.Action.STRAFE_RIGHT) {
-						controllerState.rawMove.x = value * mod;
-					}
-					else if(action == Actions.Action.TURN_LEFT) {
-						controllerState.rawLook.x = -value * mod;
-					}
-					else if(action == Actions.Action.TURN_RIGHT) {
-						controllerState.rawLook.x = -value * mod;
-					}
-					else if(action == Actions.Action.LOOK_UP) {
-						controllerState.rawLook.y = -value * mod;
-					}
-					else if(action == Actions.Action.LOOK_DOWN) {
-						controllerState.rawLook.y = -value * mod;
-					}
-				}
-				else if(binding.number == index && binding.type == type) {
-					if(action == Actions.Action.ATTACK) {
-						controllerState.attack = false;
-					}
-					else if(action == Actions.Action.DROP) {
-						controllerState.drop = false;
-					}
-					else if(action == Actions.Action.USE) {
-						controllerState.use = false;
-					}
-					else if(action == Actions.Action.MAP) {
-						controllerState.map = false;
-					}
-					else if(action == Actions.Action.PAUSE) {
-						controllerState.pause = false;
-					}
-					else if(action == Actions.Action.INVENTORY) {
-						controllerState.inventory = false;
-					}
-				}
-			}
+                    if(value > 0.25f) {
+                        controllerState.menuButtonEvents.add(ControllerState.MenuButtons.SELECT);
+                    }
+                }
+                else if(action == Actions.Action.MAP) {
+                    controllerState.map = pressed;
+                }
+                else if(action == Actions.Action.PAUSE) {
+                    controllerState.pause = pressed;
+                }
+                else if(action == Actions.Action.INVENTORY) {
+                    controllerState.inventory = pressed;
+
+                    if(value > 0.25f) {
+                        controllerState.menuButtonEvents.add(ControllerState.MenuButtons.CANCEL);
+                    }
+                }
+                else if(action == Actions.Action.FORWARD) {
+                    controllerState.rawMove.y = value * mod;
+                }
+                else if(action == Actions.Action.BACKWARD) {
+                    controllerState.rawMove.y = value * mod;
+                }
+                else if(action == Actions.Action.STRAFE_LEFT) {
+                    controllerState.rawMove.x = value * mod;
+                }
+                else if(action == Actions.Action.STRAFE_RIGHT) {
+                    controllerState.rawMove.x = value * mod;
+                }
+                else if(action == Actions.Action.TURN_LEFT) {
+                    controllerState.rawLook.x = -value * mod;
+                }
+                else if(action == Actions.Action.TURN_RIGHT) {
+                    controllerState.rawLook.x = -value * mod;
+                }
+                else if(action == Actions.Action.LOOK_UP) {
+                    controllerState.rawLook.y = -value * mod;
+                }
+                else if(action == Actions.Action.LOOK_DOWN) {
+                    controllerState.rawLook.y = -value * mod;
+                }
+            }
+            else if(binding.number == index && binding.type == type) {
+                if(action == Actions.Action.ATTACK) {
+                    controllerState.attack = false;
+                }
+                else if(action == Actions.Action.DROP) {
+                    controllerState.drop = false;
+                }
+                else if(action == Actions.Action.USE) {
+                    controllerState.use = false;
+                }
+                else if(action == Actions.Action.MAP) {
+                    controllerState.map = false;
+                }
+                else if(action == Actions.Action.PAUSE) {
+                    controllerState.pause = false;
+                }
+                else if(action == Actions.Action.INVENTORY) {
+                    controllerState.inventory = false;
+                }
+            }
 		}
+
 		return false;
 	}
 
 	@Override
 	public boolean buttonDown (Controller controller, int buttonIndex) {
 		boolean result = matchButton(buttonIndex, GamepadBinding.GamepadInputType.BUTTON, 0, 1);
-		return result;
+
+        dpadMenuNavigation(controller, buttonIndex);
+
+        return result;
 	}
 
 	@Override
@@ -237,53 +240,33 @@ public class GamepadManager implements ControllerListener {
 		return result;
 	}
 
-    /*@Override
-    public boolean povMoved (Controller controller, int povIndex, PovDirection value) {
-		boolean result = matchButton(povIndex, GamepadBinding.GamepadInputType.POV, value.ordinal(), 0);
+    public boolean dpadMenuNavigation(Controller controller, int buttonIndex) {
+        if (!this.menuMode) return false;
 
-    	if (this.menuMode) {
-    		switch (value) {
-				case north:
-					this.controllerState.dpadEvents.add(DPAD.UP);
-					result = true;
-					break;
+        ControllerMapping mapping = controller.getMapping();
 
-				case south:
-					this.controllerState.dpadEvents.add(DPAD.DOWN);
-					result = true;
-					break;
+        if (buttonIndex == mapping.buttonDpadUp) {
+            this.controllerState.dpadEvents.add(DPAD.UP);
+            return true;
+        }
 
-				case east:
-					this.controllerState.dpadEvents.add(DPAD.RIGHT);
-					result = true;
-					break;
+        if (buttonIndex == mapping.buttonDpadDown) {
+            this.controllerState.dpadEvents.add(DPAD.DOWN);
+            return true;
+        }
 
-				case west:
-					this.controllerState.dpadEvents.add(DPAD.LEFT);
-					result = true;
-					break;
-			}
-		}
+        if (buttonIndex == mapping.buttonDpadRight) {
+            this.controllerState.dpadEvents.add(DPAD.RIGHT);
+            return true;
+        }
 
-        return result;
-    }*/
+        if (buttonIndex == mapping.buttonDpadLeft) {
+            this.controllerState.dpadEvents.add(DPAD.LEFT);
+            return true;
+        }
 
-    /*@Override
-    public boolean xSliderMoved (Controller controller, int sliderIndex, boolean value) {
-		boolean result = matchButton(sliderIndex, GamepadBinding.GamepadInputType.SLIDER, value ? 1 : 0, 0);
-		return result;
-    }*/
-
-    /*@Override
-    public boolean ySliderMoved (Controller controller, int sliderIndex, boolean value) {
-		boolean result = matchButton(sliderIndex, GamepadBinding.GamepadInputType.SLIDER, value ? 0 : 1, 0);
-		return result;
-    }*/
-
-    /*@Override
-    public boolean accelerometerMoved (Controller controller, int accelerometerIndex, Vector3 value) {
         return false;
-    }*/
+    }
 
     @Override
     public void connected (Controller controller) {
@@ -302,27 +285,7 @@ public class GamepadManager implements ControllerListener {
     }
 
     public void mapController(Controller controller) {
-        String controllerName = controller.getName().toLowerCase();
-
-        if (controllerName.contains("microsoft corporatio controller")) {
-            controllerMapping.put(controller, new GamepadDefinition(new Xbox360OSXPad()));
-        }
-        else if (controllerName.contains("microsoft") || controllerName.contains("xbox") || controllerName.contains("360")) {
-            controllerMapping.put(controller, new GamepadDefinition(new Xbox360Pad()));
-        }
-        else if (controllerName.contains("sony")) {
-            controllerMapping.put(controller, new GamepadDefinition(new SonyPad()));
-        }
-        /*else if (controller.getName().equals(Ouya.ID)) {
-            controllerMapping.put(controller, new GamepadDefinition(new Ouya()));
-        }*/
-        else if (controllerName.contains("moga")) {
-            controllerMapping.put(controller, new GamepadDefinition(new MogaProPad()));
-        }
-        else {
-            controllerMapping.put(controller, new GamepadDefinition(new Xbox360Pad()));
-        }
-
+        controllerMapping.put(controller, new GamepadDefinition(controller));
         Options.SetGamepadBindings(controllerMapping.get(controller));
     }
 
