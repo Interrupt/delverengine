5635727f568f37273767fa0b2fba7fa3ff3cb52c
Joshua Skelton <joshua.skelton@gmail.com>
2025-10-11 17:14:22 -0700

    Initial work on iOS platform

---
+++ ios/src/main/java/com/interrupt/dungeoneer/IOSLauncher.java
@@ -0,0 +1,23 @@
+package com.interrupt.dungeoneer;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.interrupt.dungeoneer.GameApplication;
+
+/** Launches the iOS (RoboVM) application. */
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration configuration = new IOSApplicationConfiguration();
+        return new IOSApplication(new GameApplication(), configuration);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
